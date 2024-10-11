05a03cbf2de9bec6ccf3ccc4060d52a854cd6802
Contaldi-Quinn-1103 <98000942+Contaldi-Quinn-1104@users.noreply.github.com>
2024-10-11 12:33:23 -0700

    Goose is moving
    So the goose moves!

---
+++ Assets/Scripts/Hazards/Goose/GooseMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+
+public class GooseMove : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = Vector3.MoveTowards(transform.position, worldPosition + new Vector3(0.6f,0,0),speed * Time.deltaTime);
+    }
+}

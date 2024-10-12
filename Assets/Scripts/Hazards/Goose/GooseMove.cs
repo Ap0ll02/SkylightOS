5eedb927f4254f9a98f9b6c09573701702bbaf21
Contaldi-Quinn-1103 <98000942+Contaldi-Quinn-1104@users.noreply.github.com>
2024-10-12 16:39:02 -0700

    So we can all pull the newest version
    YO YO YO it was great working with you all. I feel like this was a very productive meeting. I am excited for the future of our game

--- Assets/Scripts/Hazards/Goose/GooseMove.cs
+++ Assets/Scripts/Hazards/Goose/GooseMove.cs
@@ -21,7 +21,7 @@ public class GooseMove : MonoBehaviour
     {
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 mousePos = Input.mousePosition;
-        mousePos.z = Camera.main.nearClipPlane;
+        mousePos.z = Camera.main.nearClipPlane; //this is so the mouse position is not outside of the camera folcrium
         worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
         transform.position = Vector3.MoveTowards(transform.position, worldPosition + new Vector3(0.6f,0,0),speed * Time.deltaTime);
     }

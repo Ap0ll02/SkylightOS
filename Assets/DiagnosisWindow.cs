663ad26b4d010bf2c08ea1470deb5dcdadfed735
Garrett Sharp <sharpgarrett908@gmail.com>
2024-11-03 23:27:40 -0800

    Comments

--- Assets/DiagnosisWindow.cs
+++ Assets/DiagnosisWindow.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Garrett Sharp
+/// Diagnosis window
+/// Script currently only needed so that reference can be grabbed.
+/// Any unique behavior that is not like a window should be implemented here though
+/// </summary>
 public class DiagnosisWindow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

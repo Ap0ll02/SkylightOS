3cef98f785e04196d0e7f4933fc76e12371d07d6
Garrett Sharp <sharpgarrett908@gmail.com>
2024-11-10 01:42:19 -0800

    OS Manager Finish Task

--- Assets/Scripts/OS Manger(Level Container)/OSManager.cs
+++ Assets/Scripts/OS Manger(Level Container)/OSManager.cs
@@ -15,10 +15,17 @@ public class OSManager : MonoBehaviour
     // Parent transform to hold the buttons
     [SerializeField] public Transform buttonContainer;
 
+    // Reference to the current task
+    [SerializeField] public AbstractTask currentTask;
+
+    // List to hold the task buttons
+    private List<Button> taskButtons = new List<Button>();
+
     // Start is called before the first frame update
     void Start()
     {
         CreateTaskButtons();
+        SubscribeToTaskEvents();
     }
 
     // Update is called once per frame
@@ -32,12 +39,35 @@ public class OSManager : MonoBehaviour
     {
         if (taskIndex >= 0 && taskIndex < tasks.Count)
         {
-            tasks[taskIndex].gameObject.SetActive(true);
-            tasks[taskIndex].startTask();
+            currentTask = tasks[taskIndex];
+            currentTask.gameObject.SetActive(true);
+            currentTask.startTask();
+
+            // Disable all buttons
+            foreach (var button in taskButtons)
+            {
+                button.interactable = false;
+            }
         }
         else
         {
             Debug.LogError("Invalid task index: " + taskIndex);
+        }
+    }
+
+    // Method to finish a task based on an index
+    public void FinishTask()
+    {
+        if (currentTask != null)
+        {
+            currentTask.isComplete = true;
+            currentTask = null;
+
+            // Enable all buttons except the completed task buttons
+            for (int i = 0; i < taskButtons.Count; i++)
+            {
+                taskButtons[i].interactable = !tasks[i].isComplete;
+            }
         }
     }
 
@@ -58,6 +88,22 @@ public class OSManager : MonoBehaviour
             // Find the TaskButton script on the child object and set the task index
             TaskButton taskButtonScript = taskButton.GetComponentInChildren<TaskButton>();
             taskButtonScript.SetTaskIndex(i);
+
+            // Add the button to the list of task buttons
+            Button buttonComponent = taskButton.GetComponentInChildren<Button>();
+            taskButtons.Add(buttonComponent);
         }
+    }
+
+    // Method to subscribe to task events
+    void SubscribeToTaskEvents()
+    {
+        AbstractTask.OnTaskCompleted += FinishTask;
+    }
+
+    // Unsubscribe from events when the object is destroyed
+    void OnDestroy()
+    {
+        AbstractTask.OnTaskCompleted -= FinishTask;
     }
 }

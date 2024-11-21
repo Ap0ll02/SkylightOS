9fce8e982f32ab45077f36918d29ba6a7dbf8edc
Apollo <jackratermann@gmail.com>
2024-11-20 22:52:26 -0800

    Coding So Fire I am Loving It
    Cloud 9, feel like a real developer cant wait for a bug to destroy all of the confidence

--- Assets/Scripts/arrowgame.cs
+++ Assets/Scripts/arrowgame.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,28 +11,53 @@ public class Arrowgame : AbstractMinigame
     /// and components.
     public GameObject arrow;
     public Transform arrowDir;
-
-    /// @var numArrows is the number of arrows to spawn on the screen.
-    int numArrows = 20;
-
-        public enum Direction {
+    public Transform parent;
+    public enum Direction {
         Up,
-        Left,
         Down,
+        Left,
         Right
     }
+    public List<Direction> arrowDirs = new();
+
+    /// @var numArrows is the number of arrows to spawn on the screen.
+    public readonly int numArrows = 20;
 
     public void Awake() {
         arrow = GameObject.Find("Arrow");
         arrowDir = arrow.GetComponent<Transform>();
+        parent = GameObject.Find("ArrowGame").GetComponent<Transform>();
+        arrowDirs.Insert(0, Direction.Right);
     }
 
     public void Start() {
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
+        SpawnArrow(numArrows);
     }
 
-    public void SpawnArrow(Direction direction) {
+    public void SpawnArrow(int numToSpawn) {
+        int[] dir = new int[]{0, 90, 180, 270};
 
+
+        for(int i = 1; i <= numToSpawn; i++) {
+            int dirNum = UnityEngine.Random.Range(0, 3);
+
+            static Direction P(int dir) => 
+                dir switch {
+                    0 => Direction.Right,
+                    1 => Direction.Up,
+                    2 => Direction.Left,
+                    3 => Direction.Down,
+                    _ => throw new ArgumentException("Invalid Random Number Cannot Match Enum", nameof(dir)),
+                };
+
+            GameObject newArrow = Instantiate(arrow, parent);
+            RectTransform rt = newArrow.GetComponent<RectTransform>();
+            rt.localPosition = UnityEngine.Vector3.zero;
+            rt.localRotation = UnityEngine.Quaternion.Euler(0, 0, dir[dirNum]);
+            arrowDirs.Insert(i, P(dirNum));
+        }
+        
     }
 
     public override void StartGame() {

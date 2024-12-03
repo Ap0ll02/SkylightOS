1c92421bfe84c11ab8158e5612b7e540d1aa9ee6
Apollo <jackratermann@gmail.com>
2024-12-03 10:53:23 -0800

    Prefab'd and Updates Scripts

--- Assets/Scripts/OS Elements/northstar.cs
+++ Assets/Scripts/OS Elements/northstar.cs
@@ -9,17 +9,18 @@ public class northstar : MonoBehaviour
     public GameObject icon;
     public List<AbstractTask> taskList = new();
     public GameObject osmanager;
+    public bool canClose = true;
     public void Awake() {
         RawImage[] riList = GetComponentsInChildren<RawImage>();
         persona = riList[0].gameObject.name == "NSPersona" ? riList[0].gameObject : riList[1].gameObject;
         icon = riList[1].gameObject.name == "NSPersona" ? riList[0].gameObject : riList[1].gameObject;
         AbstractTask[] tasks = GetComponents<AbstractTask>();
         taskList = tasks.ToList();
-        osmanager = GetComponent<OSManager>().gameObject;
+        // osmanager = GetComponent<OSManager>().gameObject;
     }
 
     public void Start() {
-
+        persona.SetActive(false);
     }
 
     public void OnUserSummon() {
@@ -28,5 +29,15 @@ public class northstar : MonoBehaviour
 
     public void OnAutoSummon() {
 
+    }
+
+    public void OnClick() {
+        if(persona.activeSelf == false) {
+            persona.SetActive(true);
+            OnUserSummon();
+        }
+        else if(canClose == true && persona.activeSelf == true){
+            persona.SetActive(false);
+        }
     }
 }

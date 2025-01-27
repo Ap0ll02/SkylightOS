5ba90249af0a68b70387b7ba30e79756cd1f24b5
Apollo <jackratermann@gmail.com>
2025-01-27 12:09:35 -0800

    Driver prefab, game prefab, script inits and starting layout

---
+++ Assets/Scripts/Mini Game/DriverGame.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriverGame : AbstractMinigame
+{
+    /*
+    Thoughts: 
+    ---
+    Side scroller, dino game clone with:
+    - Jump
+    - Crouch
+    - Collect points to avoid driver glitches like screen blackout etc.
+    ---
+    */
+
+    public GameObject player;
+    public GameObject bg;
+    // Consider redoing this as an array of possible obstacles to spawn.
+    public GameObject obstacle;
+
+    public float percentage = 0;
+
+    void Awake() {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void StartGame() {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Don't forget deltaTime with movement     
+    }
+}
--- Assets/Scripts/Task/DriverTask.cs
+++ Assets/Scripts/Task/DriverTask.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 /// @author Jack
 /// @brief Driver task involves the system window then the driver window. This will
 /// lead into a minigame, akin to the dino game on google.
 public class DriverTask : AbstractTask 
 {
     public GameObject system_menu;
-
+    public TMP_Text driver_desc;
+    public driver driver_script;
     // Get our references here
     void Awake() {
         taskTitle = "Update Drivers";
@@ -17,7 +19,8 @@ public class DriverTask : AbstractTask
         system_menu = FindObjectOfType(WindowCanvas).GetComponentInChildren<SystemResourcesWindow>().gameObject;
     }
     // Broken, non-interactable state loaded here
-    void Start() {
+    new void Start() {
+        driver_desc.text = "Driver's out of date. Updates required.";
         // TODO: Steps towards system menu
         // prepare what you can even though the system menu isn't complete
         // Get states ready and design the game.

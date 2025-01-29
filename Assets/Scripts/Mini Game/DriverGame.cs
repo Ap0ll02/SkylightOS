33756f17629565d42b6b75fd534910657d0a65ae
Apollo <jackratermann@gmail.com>
2025-01-28 22:51:15 -0800

    Updated game code, first workings of dino game started

--- Assets/Scripts/Mini Game/DriverGame.cs
+++ Assets/Scripts/Mini Game/DriverGame.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class DriverGame : AbstractMinigame
 {
@@ -14,21 +16,30 @@ public class DriverGame : AbstractMinigame
     ---
     */
 
-    public GameObject player;
+    public RectTransform player;
     public GameObject bg;
+    public GameObject bg_alt;
     // Consider redoing this as an array of possible obstacles to spawn.
     public GameObject obstacle;
 
     public float percentage = 0;
     public float chance = 0;
-    void Awake() {
+    public Vector2 bg_pos;
+    public Vector2 speed = new Vector2(-10, 0);
+    public RectTransform bg_cpos;
+    public RectTransform bg_width;
 
+    InputAction moveAction;
+
+    void Awake() {
+        bg_pos = bg.transform.position;
+        bg_cpos = bg.GetComponent<RectTransform>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");   
     }
 
     public override void StartGame() {
@@ -38,5 +49,8 @@ public class DriverGame : AbstractMinigame
     void Update()
     {
         // Don't forget deltaTime with movement     
+        bg_cpos.anchoredPosition -= speed * Time.deltaTime;
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        player.anchoredPosition += new Vector2(moveValue.x, 0);
     }
 }

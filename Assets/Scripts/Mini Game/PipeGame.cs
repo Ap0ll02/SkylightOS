138fa0858818c764652f1fcdcf31f0bc34c4ea2d
Apollo <jackratermann@gmail.com>
2025-02-07 14:15:45 -0800

    Pipe are cool we are working well

--- Assets/Scripts/Mini Game/PipeGame.cs
+++ Assets/Scripts/Mini Game/PipeGame.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PipeGame : MonoBehaviour
 {
+    public List<GameObject> PipePrefab;
+    public GameObject PipeLayout;
     // Start is called before the first frame update
     void Start()
     {

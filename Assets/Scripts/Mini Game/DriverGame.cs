140ab16f5282cd8e506813aa7eab12b77e8b638a
Apollo <jackratermann@gmail.com>
2025-02-06 20:19:11 -0800

    This works bro

--- Assets/Scripts/Mini Game/DriverGame.cs
+++ Assets/Scripts/Mini Game/DriverGame.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 using UnityEngine.InputSystem;
+
+using System;
+using TMPro;
+using Unity.VisualScripting;
+
 
 public class DriverGame : AbstractMinigame
 {
@@ -16,44 +21,99 @@ public class DriverGame : AbstractMinigame
     ---
     */
 
+    public int pCount = 0;
     public RectTransform player;
+    public player pscript;
     public GameObject bg;
-    public GameObject bg_alt;
     // Consider redoing this as an array of possible obstacles to spawn.
     public GameObject obstacle;
 
     public float percentage = 0;
-    public float chance = 0;
-    public Vector2 bg_pos;
-    public Vector2 speed = new Vector2(-10, 0);
-    public RectTransform bg_cpos;
-    public RectTransform bg_width;
+    readonly float ob_speed = 15f;
+    readonly int ob_max_spawn = 4;
+    readonly float ob_spawntime = 4f;
+
+    public GameObject parent;
+    public Vector2 speed = new(80, 0);
+    public Component[] obs;
+    public BasicWindow window;
+
+    // Spawn notes: -1200 < y < 1200
+    // x > 2300
+    public GameObject obstacle_prefab;
+    public List<GameObject> obs_list = new();
+    public List<GameObject> bgs = new();
+    public TMP_Text pBar;
 
     InputAction moveAction;
+    public CanvasGroup my_cg;
+    public Action OnGameEnd;
+    public int difficulty_p_reduction = 5;
 
     void Awake() {
-        bg_pos = bg.transform.position;
-        bg_cpos = bg.GetComponent<RectTransform>();
+        gameRunning = false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        moveAction = InputSystem.actions.FindAction("Move");   
+        my_cg.alpha = 0;
+        my_cg.interactable = false;
     }
 
     public override void StartGame() {
 
+        pBar.text = "0%";   
+        bgs.Add(Instantiate(bg, parent: parent.GetComponent<RectTransform>()));
+        bgs[^1].GetComponent<RectTransform>().anchoredPosition = new Vector3(244.5f, 0.91f, 90);
+        moveAction = InputSystem.actions.FindAction("Move");           
+        gameRunning = true;
+        my_cg.alpha = 1;
+        my_cg.interactable = true;
+        obs = obstacle.GetComponentsInChildren<RectTransform>(); 
+        pBar.text = "0%";   
+        bgs.Add(Instantiate(bg, parent: parent.GetComponent<RectTransform>()));
+        bgs[^1].GetComponent<RectTransform>().anchoredPosition = new Vector3(244.5f, 0.91f, 90);
+        obs_list.Add(Instantiate(obstacle_prefab, parent: obstacle.GetComponent<RectTransform>()));
+        StartCoroutine(Progression());
+        StartCoroutine(SpawnObstacle());
+        window.OpenWindow();
     }
+
+    public void OnEnable() {
+        pscript.OnTObs += PlayerHit;
+    }
+
+    public void OnDisable() {
+            pscript.OnTObs -= PlayerHit;
+    }
+    public void PlayerHit(Collider2D ob_hit) {
+        obs_list.Remove(ob_hit.gameObject);
+        Destroy(ob_hit.gameObject);
+        pCount -= difficulty_p_reduction;
+        pCount = pCount < 0 ? 0 : pCount;
+    }
+    public bool gameRunning = false;
     // Update is called once per frame
     void Update()
     {
-        // Don't forget deltaTime with movement     
-        bg_cpos.anchoredPosition -= speed * Time.deltaTime;
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        player.anchoredPosition += new Vector2(moveValue.x, 0);
+        if(gameRunning) {
+            // Gotta loop because ideal scrolling leaves 2 bg instances in scene
+            // at once.
+            foreach (var b in bgs) {
+                try {
+                    b.GetComponent<RectTransform>().anchoredPosition -= speed * Time.deltaTime;
+                } catch (Exception e) {
+                    Debug.Log(e);
+                }
+            }
+            
+            Vector2 moveValue = moveAction.ReadValue<Vector2>();
+            player.anchoredPosition += moveValue;
+            CheckBounds();
+            HandleObs();
+        }
     }
-    // COment for merhe
 
     void CheckBounds() {
         // Player bounds, so they can't go off screen

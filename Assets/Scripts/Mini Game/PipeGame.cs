d7ffe6f4a22cfadf2195c303ea0df08fdcfe1c97
Apollo <jackratermann@gmail.com>
2025-02-07 23:42:51 -0800

    More piping hot updates

--- Assets/Scripts/Mini Game/PipeGame.cs
+++ Assets/Scripts/Mini Game/PipeGame.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PipeGame : MonoBehaviour
 {
     public List<GameObject> PipePrefab;
+    public List<GameObject> SpawnedPipes;
     public GameObject PipeLayout;
     // Start is called before the first frame update
     void Start()
     {
-        
+        System.Random rnd = new();
+        for(int i = 0; i < 99; i++){
+            SpawnedPipes.Add(Instantiate(PipePrefab[rnd.Next(0, PipePrefab.Count)], parent: PipeLayout.GetComponent<RectTransform>()));
+        } 
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Mini Game/pipe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipe : MonoBehaviour
+{
+    public enum PipeType {
+        StraightUp,
+        StraightLaying,
+        TopRight,
+        TopLeft,
+        BottomLeft,
+        BottomRight
+    }
+
+    public int angleStyle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

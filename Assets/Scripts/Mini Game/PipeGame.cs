756adb8b5dc9fa679a4e13061385c9dd935453e4
Apollo <jackratermann@gmail.com>
2025-02-08 21:24:12 -0800

    Pipes be rotating and spawning at random

--- Assets/Scripts/Mini Game/PipeGame.cs
+++ Assets/Scripts/Mini Game/PipeGame.cs
@@ -1,19 +1,134 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
-
-public class PipeGame : MonoBehaviour
+using UnityEngine.InputSystem;
+// TODO 87 is ending pipe. 0 is first
+public class PipeGame : AbstractMinigame
 {
     public List<GameObject> PipePrefab;
     public List<GameObject> SpawnedPipes;
     public GameObject PipeLayout;
-    // Start is called before the first frame update
-    void Start()
+
+    public void Start(){
+        StartGame();
+    }
+
+   public override void StartGame()
     {
         System.Random rnd = new();
         for(int i = 0; i < 99; i++){
-            SpawnedPipes.Add(Instantiate(PipePrefab[rnd.Next(0, PipePrefab.Count)], parent: PipeLayout.GetComponent<RectTransform>()));
+            int pipeType = rnd.Next(0, 7);
+           switch (pipeType)
+            {
+                case 0: // Vertical Pipe
+                    SpawnedPipes.Add(Instantiate(PipePrefab[pipeType], parent: PipeLayout.GetComponent<RectTransform>()));
+
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.StraightUp;
+
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.FourWay);
+                   break;
+                case 1: // Horizontal Pipe
+                    SpawnedPipes.Add(Instantiate(PipePrefab[0], parent: PipeLayout.GetComponent<RectTransform>()));
+                    SpawnedPipes[i].GetComponent<RectTransform>().transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90)); 
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.StraightLaying;
+                    
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.FourWay);
+                    break;
+                case 2: // Top right pipe
+                    SpawnedPipes.Add(Instantiate(PipePrefab[1], parent: PipeLayout.GetComponent<RectTransform>()));
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.TopRight;
+
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.FourWay);
+                    break;
+                case 3:
+                    SpawnedPipes.Add(Instantiate(PipePrefab[1], parent: PipeLayout.GetComponent<RectTransform>()));
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.TopLeft;
+
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.FourWay);
+                    break;
+                case 4:
+                    SpawnedPipes.Add(Instantiate(PipePrefab[1], parent: PipeLayout.GetComponent<RectTransform>()));
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.BottomLeft;
+
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.FourWay);
+                    break;
+                case 5:
+                    SpawnedPipes.Add(Instantiate(PipePrefab[1], parent: PipeLayout.GetComponent<RectTransform>()));
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.BottomRight;
+
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.FourWay);
+                    break;
+                case 6:
+                    SpawnedPipes.Add(Instantiate(PipePrefab[2], parent: PipeLayout.GetComponent<RectTransform>()));
+                    SpawnedPipes[i].GetComponent<pipe>().PipeStyle = pipe.PipeType.FourWay;
+
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.TopLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().left_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().top_connectables.Add(pipe.PipeType.FourWay);
+
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.TopRight);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomLeft);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.StraightLaying);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.StraightUp);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.BottomRight);
+                    SpawnedPipes[i].GetComponent<pipe>().right_connectables.Add(pipe.PipeType.FourWay);
+                    SpawnedPipes[i].GetComponent<pipe>().bottom_connectables.Add(pipe.PipeType.FourWay);
+                    break;
+                default:
+                    Debug.Log("Accidental 7+");
+                    break;
+            }
         } 
     }
 
--- Assets/Scripts/Mini Game/pipe.cs
+++ Assets/Scripts/Mini Game/pipe.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class pipe : MonoBehaviour
 {
@@ -10,19 +12,53 @@ public class pipe : MonoBehaviour
         TopRight,
         TopLeft,
         BottomLeft,
-        BottomRight
+        BottomRight,
+        FourWay
     }
 
-    public List<PipeType> connectables;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public List<PipeType> top_connectables;
+    public List<PipeType> bottom_connectables;
+    public List<PipeType> left_connectables;
+    public List<PipeType> right_connectables;
+
+    public PipeType PipeStyle;
+    public InputAction rotAction;
+    void Awake(){
+        if(rotAction != null){
+            rotAction.performed += OnRotate;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Rotate the given pipe 90 degrees
+    private void OnRotate(InputAction.CallbackContext context)
     {
-        
+        if(!MouseOverPipe()) return;
+        RectTransform rt = GetComponent<RectTransform>();
+        Vector3 newRotation = rt.eulerAngles;
+        newRotation.z += 90;
+        rt.rotation = Quaternion.Euler(newRotation);
+    }
+    // Raycast results to ensure specific pipe is targeted.
+    bool MouseOverPipe(){
+        PointerEventData pd = new PointerEventData(EventSystem.current)
+        {
+            position = Mouse.current.position.ReadValue()
+        };
+        var results = new System.Collections.Generic.List<RaycastResult>();
+        EventSystem.current.RaycastAll(pd, results);
+
+        foreach(var result in results){
+            if(result.gameObject == gameObject){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void OnEnable(){
+        rotAction?.Enable();
+    }
+    void OnDisable(){
+        rotAction?.Disable();
     }
 }

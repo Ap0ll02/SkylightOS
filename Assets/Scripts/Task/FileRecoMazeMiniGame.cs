68db596ca13bcf61660a2ad05d55e5b47b78ddf8
Apollo <jackratermann@gmail.com>
2025-02-18 23:13:52 -0800

    Better

--- Assets/Scripts/Task/FileRecoMazeMiniGame.cs
+++ Assets/Scripts/Task/FileRecoMazeMiniGame.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FileRecoMazeMiniGame : AbstractMinigame
 {
-    public int mazeWidth = 10;
-    public int mazeHeight = 10;
     public GameObject wallPrefab;
     public GameObject pathPrefab;
     public GameObject parentMaze;
@@ -19,40 +17,9 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         StartGame();
     }
 
-    public override void StartGame()
-    {
-        CreateMaze();
+    public override void StartGame() {
+        
     }
 
-
-    // Github COPilot helped with automatic maze generation and camera setup
-    // 3d Maze
-    public int evIndex = 0;
-    void CreateMaze() {
-        System.Random rand = new();
-        int PathOrWall = rand.Next(0,2);
-
-        for (int i = 0; i < 840; i++){
-            if(evIndex == 0 || (i - mazePieces.IndexOf(filePiece[evIndex]) > rand.Next(90, 150) && evIndex < 5)){
-                    filePiece.Add(Instantiate(filePrefab, parent: parentMaze.GetComponent<RectTransform>()));
-                    mazePieces.Add(filePiece[evIndex]);
-                    evIndex++;
-                    i--;
-                    continue;
-            }
-
-            switch(PathOrWall){
-                case 0:
-                    mazePieces.Add(Instantiate(wallPrefab, parent: parentMaze.GetComponent<RectTransform>()));
-                    break;
-                case 1:
-                    mazePieces.Add(Instantiate(pathPrefab, parent: parentMaze.GetComponent<RectTransform>()));
-                    break;
-                default:
-                    break;
-            }
-
-            PathOrWall = rand.Next(0, 2);
-        }
-    }
+    
 }

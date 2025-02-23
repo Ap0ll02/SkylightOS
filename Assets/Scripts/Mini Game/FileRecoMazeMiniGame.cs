cf20ef225fd44152414cde338a67242964145322
Apollo <jackratermann@gmail.com>
2025-02-22 23:56:10 -0800

    Still trying to get maze boundaries to work

--- Assets/Scripts/Mini Game/FileRecoMazeMiniGame.cs
+++ Assets/Scripts/Mini Game/FileRecoMazeMiniGame.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
-using UnityEngine.UI;
+
+//TODO: Currently, the player can still move out of the maze.
 
 public class FileRecoMazeMiniGame : AbstractMinigame
 {
@@ -28,7 +28,7 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         StartGame();
     }
 
-    // Open the window, start update coroutine
+    // MARK: - Game Initialization
     public override void StartGame() {
         foreach(var maze in mazeContainer){
             maze.SetActive(false);
@@ -36,7 +36,8 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         System.Random rand = new();
         int pickMaze = rand.Next(0, mazeContainer.Count);
         mazeContainer[pickMaze].SetActive(true);
-        // SetCollider();
+        Canvas.ForceUpdateCanvases(); // Co-Pilot tip for updating UI before calculations
+        SetColliders(); // Sets the boxcolliders automatically, much love, kisses, thanks for the help copilot
         GetComponent<BasicWindow>().OpenWindow();        
         gameRunning = true;
         moveAction = InputSystem.actions.FindAction("Move"); 
@@ -49,6 +50,7 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         GetComponent<BasicWindow>().CloseWindow();
     }
     // -2645 x, 1330 y, is the maximum right and bottom we allow the maze to move
+    // MARK: - Game Update
     public IEnumerator GameUpdate() {
         RectTransform mazeRect = mazePath.GetComponent<RectTransform>();
 
@@ -57,82 +59,68 @@ public class FileRecoMazeMiniGame : AbstractMinigame
 
             // Predict the new position
             Vector2 newPos = mazeRect.anchoredPosition - moveSpeed * moveValue;
+            Vector2 playerPos = player.anchoredPosition + moveSpeed * moveValue;
 
-            // Check if the new position collides with any walls
-            if (Touching(player.gameObject, mazePath)) {
-                Debug.Log("Colliding with walls");
+            // Check if the new position stays wihtin path
+            if (Touching(player.gameObject, mazePath, playerPos)) {
                 mazeRect.anchoredPosition = newPos;
-            } else {
-                mazeRect.anchoredPosition += moveSpeed * moveValue;
             }
-
             yield return null;
         }
     }
 
-    // public bool IsCollidingWithWalls(Vector2 newPos) {
-    //     RectTransform playerRect = player.GetComponent<RectTransform>();
-
-    //     foreach (RectTransform wall in mazePath.transform) { // Assuming walls are children of mazePath
-    //         if (wall == null) continue;
-
-    //         if (RectOverlaps(playerRect, wall, newPos)) {
-    //             return true; // Collision detected, movement should be blocked
-    //         }
-    //     }
-
-    //     return false; // No collision, allow movement
-    // }
-
-    // public bool RectOverlaps(RectTransform rectA, RectTransform rectB, Vector2 newPos) {
-    //     Vector2 sizeA = rectA.sizeDelta;
-    //     Vector2 sizeB = rectB.sizeDelta;
-
-    //     Vector2 minA = newPos - sizeA / 2;
-    //     Vector2 maxA = newPos + sizeA / 2;
-
-    //     Vector2 minB = rectB.anchoredPosition - sizeB / 2;
-    //     Vector2 maxB = rectB.anchoredPosition + sizeB / 2;
-
-    //     return minA.x < maxB.x && maxA.x > minB.x &&
-    //             minA.y < maxB.y && maxA.y > minB.y;
-    // }
-
-   
-
+    // MARK: - Collision Detection
     // Use Rigidbody2D and Collider2D to detect collisions
-    public bool Touching(GameObject player, GameObject mazePath) {
+    public bool Touching(GameObject player, GameObject mazePath, Vector2 newPos) {
+        RectTransform playerRect = player.GetComponent<RectTransform>();
+        RectTransform mazeRect = mazePath.GetComponent<RectTransform>();
+
+        // Convert the player's new position to the local space of the maze
+        Vector2 localPlayerPos = mazeRect.InverseTransformPoint(playerRect.TransformPoint(newPos));
+
+        // Check if the player's new position is within the bounds of the maze
+        Vector2 mazeMin = mazeRect.rect.min;
+        Vector2 mazeMax = mazeRect.rect.max;
+
+        Vector2 playerMin = localPlayerPos - playerRect.rect.size / 2;
+        Vector2 playerMax = localPlayerPos + playerRect.rect.size / 2;
+
+        if (playerMin.x < mazeMin.x || playerMax.x > mazeMax.x || playerMin.y < mazeMin.y || playerMax.y > mazeMax.y) {
+            return false; // Player is out of bounds
+        }
+
+        // Check if the player is touching any maze colliders
         Collider2D playerCollider = player.GetComponent<BoxCollider2D>();
         Collider2D[] mazeColliders = mazePath.GetComponentsInChildren<BoxCollider2D>();
 
         foreach (Collider2D mazeCollider in mazeColliders) {
-            //if (mazeCollider == mazePath.GetComponent<Collider2D>()) continue; // Skip the parent mazePath Collider2D
             if (playerCollider.IsTouching(mazeCollider)) {
                 return true;
             }
         }
+
         return false;
     }
 
-    // public void SetCollider() { 
-    //     Canvas.ForceUpdateCanvases(); // Ensure UI is updated before calculations
+    public void SetColliders() { 
+        Canvas.ForceUpdateCanvases(); // Ensure UI is updated before calculations
 
-    //     foreach (BoxCollider2D mp in mazePath.GetComponentsInChildren<BoxCollider2D>()) { 
-            
-    //         if (mp.TryGetComponent<RectTransform>(out var rt)) {
-    //             Vector3[] corners = new Vector3[4];
-    //             rt.GetWorldCorners(corners); // Get the 4 world-space corners
+        foreach (BoxCollider2D collider in mazePath.GetComponentsInChildren<BoxCollider2D>()) { 
+            if (collider.TryGetComponent<RectTransform>(out var rt)) { // Finally get to use out variables woohoo
+                Vector3[] corners = new Vector3[4];
+                rt.GetWorldCorners(corners); // Get the 4 world-space corners
 
-    //             float width = Vector2.Distance(corners[0], corners[3]); // Left to Right
-    //             float height = Vector2.Distance(corners[0], corners[1]); // Bottom to Top
+                // Calculate the size in world space
+                float width = Vector3.Distance(corners[0], corners[3]); // Left to Right
+                float height = Vector3.Distance(corners[0], corners[1]); // Bottom to Top
 
-    //             Vector2 worldSize = new Vector2(width, height);
+                // Convert world size to local size, this part is critical, as the nesting makes world scale pointless
+                Vector2 localSize = new(width / rt.lossyScale.x, height / rt.lossyScale.y);
 
-    //             mp.size = worldSize;
-    //             Debug.Log($"Collider Set: {mp.gameObject.name} | World Size: {worldSize}");
-    //         }
-    //     }
-    // }
-
-
+                collider.size = localSize;
+                collider.offset = rt.rect.center;
+                // Debug.Log($"Collider Set: {collider.gameObject.name} | Local Size: {localSize}");
+            }
+        }
+    }
 }

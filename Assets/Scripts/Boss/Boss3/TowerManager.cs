8185a00dd8b0efd8b3424ff5bcd1267dcd0917df
Apollo <jackratermann@gmail.com>
2025-03-24 13:50:46 -0700

    Tower Manager Create and Place Tower Minor Updates

--- Assets/Scripts/Boss/Boss3/TowerManager.cs
+++ Assets/Scripts/Boss/Boss3/TowerManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+///
+/// @author: Jack Ratermann
+/// @brief: This is the managing class for towers
+/// handling placement, spawning, upgrading, etc.
 public class TowerManager : MonoBehaviour
 {
     // Tower Management: Placing towers, making sure money/payment
@@ -13,13 +17,22 @@ public class TowerManager : MonoBehaviour
     public float maxDistance = 50f;
     public bool placeMode = false;
     public Tower towerSc;
+    public GameObject pickedTower;
+
+    public List<GameObject> PlayerTowers = new();
+
+    void Start()
+    {
+        targetLayer = LayerMask.GetMask("TowerPlacements");
+    }
 
     public void PlaceTower()
     {
+        GameObject hit;
         if (placeMode)
         {
             // Check raycast for any valid place points
-            if (CheckPlacePos(out var hit))
+            if (CheckPlacePos(out hit))
             {
                 Debug.Log("Hit This Bitch: " + hit);
                 CreateTower();
@@ -27,20 +40,24 @@ public class TowerManager : MonoBehaviour
         }
     }
 
-    public bool CheckPlacePos(out var hitObject)
+    public bool CheckPlacePos(out GameObject hitObject)
     {
+        // Raycast to check mouse position, if it is hitting
+        // any 'block' in the valid layer, for ability to place tower
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, targetLayer))
         {
             Debug.Log("Success");
+            hitObject = hit.collider.gameObject;
             return true;
         }
-        else
-            return false;
+        hitObject = null;
+        return false;
     }
 
     public void CreateTower()
     {
         Debug.Log("Create Tower");
+        PlayerTowers.Add(Instantiate(pickedTower, parent: this.GetComponent<Transform>()));
     }
 }

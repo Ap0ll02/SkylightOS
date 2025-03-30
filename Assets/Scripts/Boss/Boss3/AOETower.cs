d1ee31b0e7c9aea1d36360e0f23509667896ac33
Apollo <jackratermann@gmail.com>
2025-03-30 14:14:06 -0700

    Tower Prefab Updates

--- Assets/Scripts/Boss/Boss3/AOETower.cs
+++ Assets/Scripts/Boss/Boss3/AOETower.cs
@@ -1,10 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AOETower : Tower
 {
-  public override void Attack() {
-    Debug.Log("Attack");
-  }
+    public GameObject projectilePrefab;
+    public GameObject projectile;
+
+    public override void Attack()
+    {
+        Debug.Log("Attack");
+    }
 }
--- Assets/Scripts/Boss/Boss3/BasicTower.cs
+++ Assets/Scripts/Boss/Boss3/BasicTower.cs
@@ -1,16 +1,45 @@
+using System.Collections;
 using UnityEngine;
 
 public class BasicTower : Tower
 {
     public GameObject projectilePrefab;
     public GameObject projectile;
+    private SphereCollider mySphere;
+    public Coroutine ActivateTower;
+    public int attackRadius;
+    public GameObject targetEnemy;
+
+    public void Start()
+    {
+        mySphere = GetComponent<SphereCollider>();
+        mySphere.radius = attackRadius;
+        ActivateTower = StartCoroutine(ActiveTower());
+    }
+
+    public IEnumerator ActiveTower()
+    {
+        yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("tdEnemy"))
+        {
+            Debug.Log("Enemy Here");
+            Debug.Log(
+                "Stats: "
+                    + GetComponent<Transform>().position
+                    + " with pos: "
+                    + other.GetComponent<Transform>().position
+            );
+            Attack();
+        }
+    }
 
     public override void Attack()
     {
         Debug.Log("Attack");
-        projectile = Instantiate(
-            projectilePrefab,
-            parent: this.gameObject.GetComponent<Transform>()
-        );
+        projectile = Instantiate(projectilePrefab, parent: GetComponent<Transform>());
     }
 }

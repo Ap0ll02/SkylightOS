78c7fb7eae0b29d6c8d3453e814d768df974a758
Apollo <jackratermann@gmail.com>
2025-04-19 23:28:18 -0700

    Small Change Here and dumb neovim fix lmao

--- Assets/Scripts/Task/TerminalTask.cs
+++ Assets/Scripts/Task/TerminalTask.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 /**
  * @Author Jack Ratermann
@@ -12,7 +12,7 @@ using TMPro;
  * @Note If this breaks, the likely cause is "TerminalWindow" in the scene wasn't renamed and kept its' original "TerminalWindow Variant" name.
  */
 
-public class TerminalTask : AbstractTask 
+public class TerminalTask : AbstractTask
 {
     /// @var State Enum that has on and off state for the task.
     private enum State
@@ -23,7 +23,7 @@ public class TerminalTask : AbstractTask
         ArrowGameOff,
         MazeGameOn,
         MazeGameOff,
-        StageTwo
+        StageTwo,
     }
 
     /// @var Terminal Variables that get references in awake() to the scene's terminal
@@ -60,18 +60,24 @@ public class TerminalTask : AbstractTask
 
     // TODO: The terminal window itself is dependent on this task heavily
     // we need to refactor this, which would also allow other tasks to be activated from here
-    [SerializeField] BasicWindow window;
+    [SerializeField]
+    BasicWindow window;
 
     /// @brief Assign all of the terminal objects in the scene.
     public void Awake()
     {
         taskTitle = "Download and Install Antivirus";
-        taskDescription = "Click to download the Antivirus Toolkit, and then access the files to install it.";
+        taskDescription =
+            "Click to download the Antivirus Toolkit, and then access the files to install it.";
         arrowGame = FindObjectOfType<Arrowgame>().gameObject;
-        terminal = FindObjectOfType<Terminal>().gameObject.GetComponentInParent<BasicWindow>().gameObject;
+        terminal = FindObjectOfType<Terminal>()
+            .gameObject.GetComponentInParent<BasicWindow>()
+            .gameObject;
         terminalPanel = FindObjectOfType<Terminal>().gameObject;
         terminalText = terminalPanel.GetComponentInChildren<TMP_Text>();
-        hGroup = FindObjectOfType<Terminal>().gameObject.GetComponentInChildren<HorizontalLayoutGroup>().gameObject;
+        hGroup = FindObjectOfType<Terminal>()
+            .gameObject.GetComponentInChildren<HorizontalLayoutGroup>()
+            .gameObject;
         termState = State.Off;
         ag = arrowGame.GetComponent<Arrowgame>();
         drawMaze = FindObjectOfType<DrawMaze>().gameObject;
@@ -84,8 +90,9 @@ public class TerminalTask : AbstractTask
         installBtn = GameObject.Find("InstallBtn");
         installBtn.GetComponent<Button>().onClick.AddListener(CompleteTask);
     }
-    
-    public new void Start() {
+
+    public new void Start()
+    {
         window.ForceCloseWindow();
         base.Start();
         installBtn.SetActive(false);
@@ -119,7 +126,8 @@ public class TerminalTask : AbstractTask
         PerformanceThiefManager.PThiefUpdate -= PThiefUpdate;
     }
 
-    void TimerOn() {
+    void TimerOn()
+    {
         iTimer = false;
     }
 
@@ -127,7 +135,8 @@ public class TerminalTask : AbstractTask
     {
         foreach (var hazardManager in hazardManagers)
         {
-            if (!hazardManager.CanProgress()) {
+            if (!hazardManager.CanProgress())
+            {
                 ag.CanContinue = false;
                 break;
             }
@@ -147,23 +156,30 @@ public class TerminalTask : AbstractTask
     public override void startTask()
     {
         // Terminal Task Start, Prompts User To Use The AntiVirus installation tool. Changes terminal state to On.
-        string termText = "Welcome To The Console, Let's get you started downloading some software\n"
-            + "The AntiVirus toolkit is a helpful addition for getting rid of pesky malware!\n" + "Click On The Anti-Virus Download to start.";
-        northstar.WriteHint("Let's try clicking the Download Anti-Virus button, open up the terminal first!", Northstar.Style.chilly);
+        string termText =
+            "Welcome To The Console, Let's get you started downloading some software\n"
+            + "The AntiVirus toolkit is a helpful addition for getting rid of pesky malware!\n"
+            + "Click On The Anti-Virus Download to start.";
+        northstar.WriteHint(
+            "Let's try clicking the Download Anti-Virus button, open up the terminal first!",
+            Northstar.Style.chilly
+        );
         termState = State.On;
         terminalText.text = termText;
     }
 
     public override void stopHazards()
     {
-        foreach (var hazardManager in hazardManagers) {
+        foreach (var hazardManager in hazardManagers)
+        {
             hazardManager.StopHazard();
         }
     }
 
     public override void startHazards()
     {
-        foreach (var hazardManager in hazardManagers) {
+        foreach (var hazardManager in hazardManagers)
+        {
             hazardManager.StartHazard();
         }
     }
@@ -171,7 +187,7 @@ public class TerminalTask : AbstractTask
     /// @brief 3 Functions To Handle When The Terminal Buttons Are Pressed.
     public void AVTask()
     {
-        /// @var termLoadBar will be used for arrow game, how the loading progresses. 
+        /// @var termLoadBar will be used for arrow game, how the loading progresses.
         /// Different process and visual than the prefab loading bar.
         if (termState == State.On)
         {
@@ -182,29 +198,36 @@ public class TerminalTask : AbstractTask
             //hGroup.SetActive(false);
             ag.StartGame();
             agInput = ag.pInput;
-            if(!musicAG.isPlaying) {
+            if (!musicAG.isPlaying)
+            {
                 musicAG.Play();
             }
         }
-        else if (termState == State.ArrowGameOn) {
+        else if (termState == State.ArrowGameOn)
+        {
             terminalText.text = "";
         }
-        else if (termState == State.MazeGameOn) {
+        else if (termState == State.MazeGameOn)
+        {
             terminalText.text = terminalText.text;
         }
     }
 
     // Used for when the games are done
-    public void GameEnd() {
-        if(termState == State.ArrowGameOn) {
+    public void GameEnd()
+    {
+        if (termState == State.ArrowGameOn)
+        {
             termState = State.ArrowGameOff;
             arrowGame.SetActive(false);
             hGroup.SetActive(true);
             StartCoroutine(FadeOut(5));
-            terminalText.text = "Download Complete. Please Locate The Installer In The Filesystem.\n";
+            terminalText.text =
+                "Download Complete. Please Locate The Installer In The Filesystem.\n";
             terminalText.text += "Try our new file browser with the button below!\n";
         }
-        else if(termState == State.MazeGameOn) {
+        else if (termState == State.MazeGameOn)
+        {
             termState = State.MazeGameOff;
             hGroup.SetActive(true);
             dm.gameObject.SetActive(false);
@@ -220,7 +243,7 @@ public class TerminalTask : AbstractTask
             {
                 State.MazeGameOff => 1,
                 State.ArrowGameOff => 0,
-                _ => -1
+                _ => -1,
             };
 
         tasksDone[getInd(termState)] = true;
@@ -229,10 +252,11 @@ public class TerminalTask : AbstractTask
     // Start of the maze game task with state checking so the LS can be used in multiple ways, further
     // decoupling the task from the OS element to enforce reusability & individuality of tasks and components
     Coroutine info;
+
     public void LSTask()
     {
         Debug.Log(termState);
-        if(termState == State.ArrowGameOff)
+        if (termState == State.ArrowGameOff)
         {
             dm.gameObject.SetActive(true);
             termState = State.MazeGameOn;
@@ -246,25 +270,30 @@ public class TerminalTask : AbstractTask
             hGroup.SetActive(false);
             dm.StartGame();
         }
-        else if (termState == State.ArrowGameOn) {
+        else if (termState == State.ArrowGameOn)
+        {
             terminalText.text = "";
         }
-        else if (termState == State.MazeGameOn) {
+        else if (termState == State.MazeGameOn)
+        {
             terminalText.text = terminalText.text;
         }
     }
 
     void NMAPTask()
     {
-        if (termState == State.MazeGameOn) {
+        if (termState == State.MazeGameOn)
+        {
             terminalText.text = terminalText.text;
         }
-        else if (termState == State.ArrowGameOn) {
+        else if (termState == State.ArrowGameOn)
+        {
             terminalText.text = "";
         }
     }
 
-    public new void CompleteTask() {
+    public new void CompleteTask()
+    {
         termState = State.Off;
         installBtn.SetActive(false);
         terminalText.text = "Successful Install Of Anti-Virus!";
@@ -272,20 +301,26 @@ public class TerminalTask : AbstractTask
     }
 
     // Variable second timer for allowing text to read, or whichever your heart desires
-    private IEnumerator Timer(float x) {
-        while(true) yield return new WaitForSeconds(x);
+    private IEnumerator Timer(float x)
+    {
+        while (true)
+            yield return new WaitForSeconds(x);
     }
 
     // Fades out music, passed in parameter is number of seconds to fadeout
-    private IEnumerator FadeOut(float x) {
-        while(musicAG.volume > 0) {
-            musicAG.volume -= 1/(x*10);
+    private IEnumerator FadeOut(float x)
+    {
+        while (musicAG.volume > 0)
+        {
+            musicAG.volume -= 1 / (x * 10);
             yield return new WaitForSeconds(0.1f);
         }
-        if(musicAG.volume < 0) musicAG.Stop();  
+        if (musicAG.volume < 0)
+            musicAG.Stop();
     }
 
-    public IEnumerator StopInputTimer(float x) {
+    public IEnumerator StopInputTimer(float x)
+    {
         StopInputRunning = true;
         Debug.Log("Input Disabled");
         ag.CanContinue = false;
@@ -315,6 +350,6 @@ public class TerminalTask : AbstractTask
         // if PThief is 0.75, then the delay is (1/0.75 - 1) * 1.5 = 0.5
         // etc.
 
-        PThiefDelay = (1f/PThiefModifier - 1) * 1.5f;
+        PThiefDelay = (1f / PThiefModifier - 1) * 1.5f;
     }
 }

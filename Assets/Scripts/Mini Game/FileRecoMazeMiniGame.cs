001aa6d394743aa312cb1c18f4b27c840f6b6f5a
Garrett Sharp <sharpgarrett908@gmail.com>
2025-04-22 22:40:20 -0700

    Added Evidence

--- Assets/Scripts/Mini Game/FileRecoMazeMiniGame.cs
+++ Assets/Scripts/Mini Game/FileRecoMazeMiniGame.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 using TMPro;
 public class FileRecoMazeMiniGame : AbstractMinigame
 {
-    // Maze Initials
+    // Maze Initials  
     public GameObject player;
     public GameObject cameraPos;
     public GameObject filePiecePrefab;
@@ -16,8 +16,9 @@ public class FileRecoMazeMiniGame : AbstractMinigame
     int filePiecesCount = 5;
     public GameObject parentContainer;
     public GameObject ExitWall;
+    public GameObject finalFilePiece; // New final file piece  
 
-    // GamePlay Initials
+    // GamePlay Initials  
     public Coroutine updateGame;
     public InputAction moveAction;
     public bool gameRunning = false;
@@ -28,10 +29,12 @@ public class FileRecoMazeMiniGame : AbstractMinigame
     public event Action FileMazeStarted;
     public event Action FileMazeOver;
 
-    // Reference to the 'Files Recovered' UI element
+    // Reference to the 'Files Recovered' UI element  
     public TMP_Text filesRecoveredText;
 
-    // Get any non-inspector references here
+    public BasicWindow evidence;
+
+    // Get any non-inspector references here  
     void Awake()
     {
         gameObject.SetActive(true);
@@ -39,51 +42,63 @@ public class FileRecoMazeMiniGame : AbstractMinigame
     void Start()
     {
         GetComponent<BasicWindow>().CloseWindow();
-        // StartGame();
+        // StartGame();  
     }
 
-    // MARK: - Game Initialization
-    public override void StartGame() {
+    // MARK: - Game Initialization  
+    public override void StartGame()
+    {
         FileMazeStarted?.Invoke();
         filePieces = new List<GameObject>();
-        // Canvas.ForceUpdateCanvases(); // Co-Pilot tip for updating UI before calculations
-        GetComponent<BasicWindow>().OpenWindow();        
+        GetComponent<BasicWindow>().OpenWindow();
         HashSet<int> randomNumbers = new();
         gameRunning = true;
         SpawnList.AddRange(SpawnContainer.GetComponentsInChildren<Transform>());
         System.Random rand = new();
-        while(randomNumbers.Count < 5){
+        while (randomNumbers.Count < 5)
+        {
             randomNumbers.Add(rand.Next(0, SpawnList.Count));
         }
         int i = 0;
-        foreach(int num in randomNumbers){
+        foreach (int num in randomNumbers)
+        {
             filePieces.Add(Instantiate(filePiecePrefab, parent: parentContainer.transform));
             filePieces[i].transform.position = SpawnList[num].position;
             i++;
         }
-        foreach(Transform spawnBlock in SpawnList){
+        foreach (Transform spawnBlock in SpawnList)
+        {
             spawnBlock.gameObject.SetActive(false);
         }
-        // new Vector3(player.transform.localScale.x, player.transform.localScale.y, player.transform.localScale.z);
-        moveAction = InputSystem.actions.FindAction("Move"); 
+        moveAction = InputSystem.actions.FindAction("Move");
         updateGame = StartCoroutine(GameUpdate());
+
+        // Ensure the final file piece is disabled initially  
+        if (finalFilePiece != null)
+        {
+            finalFilePiece.SetActive(false);
+        }
     }
-    public void OnEnable() {
+
+    public void OnEnable()
+    {
         rpScript.FileRecovered += FileRecover;
         ewScript.GameOverEvent += GameOver;
     }
 
-    public void OnDisable() {
+    public void OnDisable()
+    {
         rpScript.FileRecovered -= FileRecover;
         ewScript.GameOverEvent -= GameOver;
     }
 
-    public void GameOver() {
+    public void GameOver()
+    {
         gameRunning = false;
         isStarted = false;
         StopCoroutine(updateGame);
-        // Destroy all the file pieces, the list, and the player
-        foreach(GameObject filePiece in filePieces){
+        foreach (GameObject filePiece in filePieces)
+        {
             Destroy(filePiece);
         }
         filePieces.Clear();
@@ -91,19 +106,18 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         FileMazeOver?.Invoke();
         GetComponent<BasicWindow>().CloseWindow();
     }
-    // -2645 x, 1330 y, is the maximum right and bottom we allow the maze to move
-    // MARK: - Game Update
+
     Vector3 velocity = Vector3.zero;
-    public IEnumerator GameUpdate() {
-        while (gameRunning) {
-            // Read input, move player
+    public IEnumerator GameUpdate()
+    {
+        while (gameRunning)
+        {
             Vector2 moveValue = moveAction.ReadValue<Vector2>();
             player.transform.position += moveSpeed * Time.deltaTime * (Vector3)moveValue;
             cameraPos.transform.position = Vector3.SmoothDamp(cameraPos.transform.position, player.transform.position, ref velocity, 0.1f);
-            // Rotate Player In Direction Of Movement
-            // CoPilot Help To Write Every Case After The First
             var turnSpeed = 8f * Time.deltaTime;
-            if(moveValue != Vector2.zero){
+            if (moveValue != Vector2.zero)
+            {
                 var targetRotation = Mathf.Atan2(moveValue.y, moveValue.x) * Mathf.Rad2Deg;
                 player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler(0, 0, targetRotation), turnSpeed);
             }
@@ -111,22 +125,37 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         }
     }
 
-    public void ExitUnlock() {
+    public void ExitUnlock()
+    {
         ExitWall.GetComponent<BoxCollider2D>().isTrigger = true;
+
+        // Enable the final file piece when the exit is unlocked  
+        if (finalFilePiece != null)
+        {
+            finalFilePiece.SetActive(true);
+        }
     }
 
-    public void FileRecover(Collider2D filePiece) {
-        switch(filePiecesCount){
+    public void FileRecover(Collider2D filePiece)
+    {
+        switch (filePiecesCount)
+        {
             case 1:
                 ExitUnlock();
-                if (filePieces.Contains(filePiece.gameObject)) {
+                if (filePieces.Contains(filePiece.gameObject))
+                {
                     filePieces.Remove(filePiece.gameObject);
                     Destroy(filePiece.gameObject);
                 }
                 filePiecesCount--;
                 break;
+            case 0:
+                finalFilePiece.SetActive(false);
+                evidence.OpenWindow();
+                break;
             default:
-                if (filePieces.Contains(filePiece.gameObject)) {
+                if (filePieces.Contains(filePiece.gameObject))
+                {
                     filePieces.Remove(filePiece.gameObject);
                     Destroy(filePiece.gameObject);
                 }
@@ -136,21 +165,8 @@ public class FileRecoMazeMiniGame : AbstractMinigame
         UpdateCounter();
     }
 
-    // Updates the 'Files Recovered' UI element
     public void UpdateCounter()
     {
         filesRecoveredText.text = "Files Recovered: " + (5 - filePiecesCount) + "/5";
     }
-
-    // public void Test_fileRecovery() {
-    //     Debug.Log("File Recovered: ");
-    //     FileRecover(filePieces[0].GetComponent<Collider2D>());
-    //     try {
-    //         Assert.IsTrue(filePiecesCount == 0);
-    //     } catch (AssertionException e) {
-    //         Debug.Log("FAILED" + e);
-    //         return;
-    //     }
-    //     Debug.Log("PASSED");
-    // }
 }

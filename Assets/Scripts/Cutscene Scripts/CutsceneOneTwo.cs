798e2d30357f00f2660b3a2936a224ebd64f6a0d
Apollo <jackratermann@gmail.com>
2025-04-22 21:10:57 -0700

    More Cutscene Parts

--- Assets/Scripts/Cutscene Scripts/CutsceneOneTwo.cs
+++ Assets/Scripts/Cutscene Scripts/CutsceneOneTwo.cs
@@ -10,23 +10,49 @@ public class CutsceneOneTwo : MonoBehaviour
     void Start()
     {
         newMsg.gameObject.SetActive(false);
-        StartCoroutine(FirstPart());
+        message.gameObject.SetActive(false);
+        StartCoroutine(FirstPart(9f, 1));
     }
 
-    public IEnumerator FirstPart()
+    public IEnumerator FirstPart(float seconds, int x)
     {
         while (onVar)
         {
-            yield return new WaitForSeconds(6f);
+            yield return new WaitForSeconds(seconds);
             onVar = false;
-            NewMessage();
+            Handler(x);
         }
     }
 
-    TMPro.TMP_Text newMsg;
+    public void Handler(int route)
+    {
+        switch (route)
+        {
+            case 1:
+                NewMessage();
+                break;
+            case 2:
+                DisplayMessage();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public TMPro.TMP_Text newMsg;
 
     public void NewMessage()
     {
         newMsg.gameObject.SetActive(true);
+        onVar = true;
+        StartCoroutine(FirstPart(3f, 2));
+    }
+
+    public TMPro.TMP_Text message;
+
+    public void DisplayMessage()
+    {
+        newMsg.gameObject.SetActive(false);
+        message.gameObject.SetActive(true);
     }
 }

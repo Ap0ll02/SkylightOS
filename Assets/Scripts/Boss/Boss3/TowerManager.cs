2dcba7e7dda2e802f4a8ad83065dfb3ca121516f
Apollo <jackratermann@gmail.com>
2025-04-23 19:32:35 -0700

    Quick Tower Variable Change

--- Assets/Scripts/Boss/Boss3/TowerManager.cs
+++ Assets/Scripts/Boss/Boss3/TowerManager.cs
@@ -153,6 +153,7 @@ public class TowerManager : MonoBehaviour
             else
             {
                 clicked = true;
+                pickedTower = hit.GetComponent<Tower>();
             }
         }
     }

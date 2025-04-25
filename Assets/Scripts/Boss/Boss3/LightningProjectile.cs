49d6937acf1a3ed1b969d0005d0cca54849ef669
Apollo <jackratermann@gmail.com>
2025-04-24 21:00:15 -0700

    OH yeah I fixed the projectiles

--- Assets/Scripts/Boss/Boss3/LightningProjectile.cs
+++ Assets/Scripts/Boss/Boss3/LightningProjectile.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class LightningProjectile : Projectile
 {
     Vector3 motion;
+    float smallMotionNumber = 0.11f;
     public override void Start() {
         base.Start();
         motion = transform.position;
     }
-    void Update()
+    public override void Update()
     {
+        base.Update();
         Vector3 newMotion = transform.position;
-
+        if(Mathf.Abs(Vector3.Distance(newMotion, motion)) < 0.11f) {
+            CleanUp();
+        }
+        motion = transform.position;
     }
 }

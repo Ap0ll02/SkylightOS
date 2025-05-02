794a088984fb714c2de17b6f862b8499dad76ab0
Apollo <jackratermann@gmail.com>
2025-05-01 19:36:48 -0700

    This makes the tutorial a bit better

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -5,23 +5,53 @@ using UnityEngine;
 public class TutorialScript : MonoBehaviour
 {
     CanvasGroup cg;
-    Coroutine arrowCrts;
+    Coroutine arrowCrts = null;
     void Start()
     {
         cg = gameObject.GetComponent<CanvasGroup>();
-        if (arrowCrts == null) {
-            StartCoroutine(Timer(21.5f));
-        } 
+        StartCoroutine(Timer(23f)); 
     }
 
     public IEnumerator Timer(float s) {
         yield return new WaitForSeconds(s);
-        StartCoroutine(BlinkArrow());
+        arrowCrts = StartCoroutine(BlinkArrow());
     }
+
+    public IEnumerator TimerTwo(float s) {
+        yield return new WaitForSeconds(s);
+        arrowCrts = StartCoroutine(UpArrowToo());
+    }
+
     public IEnumerator BlinkArrow() {
+        cg.alpha = 0;
         float threshhold = 0.1f;
         int blinkCount = 0;
-        while(blinkCount < 10) {
+        while(blinkCount < 120) {
+            yield return new WaitForSeconds(0.05f);
+            if(cg.alpha > threshhold){
+                cg.alpha -= 0.05f;
+            }
+            else if(cg.alpha == 1) {
+                threshhold = 0.1f;
+            }
+            else {
+                threshhold = 1f;
+                cg.alpha += 0.05f;
+            }
+            blinkCount++;
+        }
+        cg.alpha = 0;
+        StartCoroutine(TimerTwo(2f));
+    }
+
+    public IEnumerator UpArrowToo() { 
+        float threshhold = 0.1f;
+        cg.alpha = 1;
+        int blinkCount = 0;
+        RectTransform tr = GetComponent<RectTransform>();
+        tr.anchoredPosition += new Vector2(-80, 300);
+        tr.rotation = Quaternion.Euler(0, 0, 300);
+        while(blinkCount < 100) {
             yield return new WaitForSeconds(0.05f);
             if(cg.alpha > threshhold){
                 cg.alpha -= 0.05f;
